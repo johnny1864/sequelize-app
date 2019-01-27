c35dc1e91022e0dd20dbd52b26ef7f8e709c1af8
johnny1864 <johnny1864@yahoo.com>
2019-01-26 17:36:09 -0800

    added validation to add gig and added search functionality to index

--- routes/gigs.js
+++ routes/gigs.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
+const squelize = require('sequelize');
+const op = squelize.Op;
 
 // GET GIGS FROM DB
 router.get('/', (req, res) => {
@@ -11,9 +13,18 @@ router.get('/', (req, res) => {
       res.render('gigs', {
         gigs
       });
-      // res.sendStatus(200);
     })
     .catch(err => console.log(err));
+});
+
+// SEARCH ROUTE
+router.get('/search', (req, res) => {
+  let { term } = req.query;
+  term = term.toLowerCase();
+
+  Gig.findAll({ where: { technologies: { [op.like]: '%' + term + '%' } } })
+    .then(gigs => res.render('gigs', { gigs }))
+    .catch(err => console.log('error ', err));
 });
 
 //DISPLAY ADD FORM
@@ -21,23 +32,52 @@ router.get('add', (req, res) => res.render('add'));
 
 // ADD GIG TO DB
 router.post('/add', (req, res) => {
-  let data = req.body;
+  let { title, technologies, budget, description, contact_email } = req.body;
 
-  let { title, technologies, budget, description, contact_email } = data;
+  // VALIDATION
+  let errors = [];
+  if (!title) {
+    errors.push({ text: 'Please Add a Title' });
+  }
+  if (!technologies) {
+    errors.push({ text: 'Please Add a Technologies' });
+  }
+  if (!description) {
+    errors.push({ text: 'Please Add a Description' });
+  }
+  if (!budget) {
+    budget = 'Undecided';
+  }
+  if (!contact_email) {
+    errors.push({ text: 'Please Add a Email' });
+  }
 
-  // INSERT INTO TABLE
-  Gig.create({
-    title,
-    technologies,
-    budget,
-    description,
-    contact_email
-  })
-    .then(gig => res.redirect('/gigs'))
-    .catch(err => console.log(err));
+  if (errors.length > 0) {
+    res.render('add', {
+      errors,
+      title,
+      technologies,
+      budget,
+      description,
+      contact_email
+    });
+  } else {
+    technologies = technologies.toLowerCase();
 
-  // REDIRECT TO HOME PAGE
-  res.redirect('/');
+    // INSERT INTO TABLE
+    Gig.create({
+      title,
+      technologies,
+      budget,
+      description,
+      contact_email
+    })
+      .then(gig => res.redirect('/gigs'))
+      .catch(err => console.log(err));
+
+    // REDIRECT TO HOME PAGE
+    res.redirect('/');
+  }
 });
 
 module.exports = router;
